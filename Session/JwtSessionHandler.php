9873616979b1b76e941ce5fb03e18adc23fc6048
Valerian Dorcy <valerian.dorcy@gmail.com>
2018-04-03 21:31:24 +0100

    Update copyright

--- Session/JwtSessionHandler.php
+++ Session/JwtSessionHandler.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This file is part of the JwtSessionBundle package.
+ *
+ * (c) Valérian Dorcy <valerian.dorcy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
 namespace Dorcyv\JwtSessionBundle\Session;
 
 use Dorcyv\JwtSessionBundle\Cookie\CookieManager;
